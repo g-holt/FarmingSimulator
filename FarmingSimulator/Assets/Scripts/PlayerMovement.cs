a187db41edcb960733e5283486bc5f33183a6f1b
Grayson Holt <gholtwork@gmail.com>
2023-05-20 19:05:41 -0500

    Movement/Animations for Forwards/Backwards
    Added Player movement and movement animations for forward and backward

--- FarmingSimulator/Assets/Scripts/PlayerMovement.cs
+++ FarmingSimulator/Assets/Scripts/PlayerMovement.cs
@@ -5,9 +5,15 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    const string isIdle = "IsIdle";
+    const string isWalking = "IsWalking";
+    const string isWalkingBackward = "IsWalkingBackward";
+
     [SerializeField] float moveSpeed = 5f;
 
     Vector2 moveInput;
+    Vector3 newPosition;
+    Animator animator;
 
     float moveXPos;
     float moveZPos;
@@ -15,7 +21,10 @@ public class PlayerMovement : MonoBehaviour
 
     void Start()
     {
-        
+        animator = GetComponentInChildren<Animator>();
+
+        SetBool(isIdle, true);
+        SetBool(isWalking, false);
     }
 
     
@@ -33,13 +42,36 @@ public class PlayerMovement : MonoBehaviour
 
     void Move()
     {
-        if(moveInput.x == 0f && moveInput.y == 0f) { return; }
+        if(moveInput.x == 0f && moveInput.y == 0f) 
+        {
+            SetBool(isIdle, true);
+            SetBool(isWalking, false);
+            SetBool(isWalkingBackward, false);
+            return; 
+        }
 
         moveXPos = moveInput.x * moveSpeed * Time.deltaTime;
         moveZPos = moveInput.y * moveSpeed * Time.deltaTime;
+
+        newPosition = transform.position + new Vector3(moveXPos, 0f, moveZPos);
+        transform.position = newPosition;
+
+        SetBool(isIdle, false);
+        
+        if(moveInput.y < 0f)
+        {
+            SetBool(isWalkingBackward, true);
+        }
+        else
+        {
+            SetBool(isWalking, true);
+        }
     }
 
 
-
-
+    void SetBool(string animation, bool state)
+    {
+        animator.SetBool(animation, state);
+    }
+    
 }
